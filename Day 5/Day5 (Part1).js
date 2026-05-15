a1c37a5bf53bd32b314104d3ab18fc8bcd8b240e
Aditya-hit <adityamadissery@gmail.com>
2026-05-15 11:55:44 +0530

    Day 5

---
+++ Day 5/Day5 (Part1).js
@@ -0,0 +1,112 @@
+// ___________________________________________Practicle No.1________________________________________
+// for (let a = 1; a < 11; a++){
+//     console.log("Number is :" + a);
+// }
+
+// let b = 1;
+// while (b < 11){
+//     console.log("Number is :" + b);
+//     b++;
+// }
+
+// let c = 1;
+// do {
+//     console.log("Number is :" + c);
+//     c++;
+// }
+// while (c < 11);
+
+// ___________________________________________Practicle No.2________________________________________
+// let studentname = "Aditya";
+// for (d = 0; d < 5; d++){
+//     console.log("Name of the student is :" + studentname);
+// }
+
+// let studentname = "Aditya";
+// let e = 0;
+// while (e < 5){
+//     console.log("Name of the student is :" + studentname);
+//     e++;
+// }
+
+// let studentname = "Aditya";
+// let f = 0;
+// do {
+//     console.log("Name of the student is : " + studentname);
+//     f++;
+// }
+// while (f < 5)
+
+// ___________________________________________Practicle No.3________________________________________
+// const  marks = [10, 20, 30];
+// let total = 0;
+// for(let g = 0; g < marks.length;) {
+//     total += marks[g];
+//     g++;
+// }
+// console.log("Total marks is :" + total);
+
+// let h = 0;
+// let total = 0;
+// while (h < marks.length) {
+//     total += marks[h];
+//     h++;
+// }
+// console.log("Total marks is :" + total);
+
+// let i = 0;
+// let total = 0;
+// do {
+//     total += marks[i];
+//     i++;
+// } while (i < marks.length);
+// console.log ("Total marks is :" + total);
+
+// ___________________________________________Practicle No.4________________________________________
+// let number = 5;
+// for (let j = 1; j<= 10; j++ ){
+//     console.log(`${number} x ${j} = ${number * j}`);
+// }
+
+// let number = 5;
+// let k = 1; // Initialization
+// while (k <= 10) { // Condition
+//     console.log(`${number} x ${k} = ${number * k}`);
+//     k++; // Increment
+// }
+
+// let number = 5;
+// let l = 1;
+// do {
+//     console.log(`${number} x ${l} = ${number * l}`);
+//     l++;
+// } while (l <= 10);
+
+// ___________________________________________Practicle No.5________________________________________
+// for ( let m = 1; m <= 10; m++) {
+//     if ( m % 2 === 0 ){
+//         console.log(m + " is Even");
+//     } else {
+//         console.log(m + " is Odd");
+//     }
+// }
+
+// let n = 1;
+// while (n <= 10) {
+//   if (n % 2 === 0) {
+//     console.log(n + " is Even");
+//   } else {
+//     console.log(n + " is Odd");
+//   }
+//   n++; 
+// }
+
+let o = 1;
+do {
+  if (o % 2 === 0) {
+    console.log(o + " is Even");
+  } else {
+    console.log(o + " is Odd");
+  }
+  o++;
+} while (o <= 10);
