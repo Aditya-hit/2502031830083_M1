097687ae36b5d90c37c6588784c283682758f145
Aditya-hit <adityamadissery@gmail.com>
2026-05-21 12:45:57 +0530

    Day 9

--- React JS/my-app/src/hello.jsx
+++ React JS/my-app/src/hello.jsx
@@ -1,5 +1,33 @@
 function hello() {
-    return <h2>Hello World</h2>
+    
+    const getName = (yourname) =>{
+        return yourname;
+    }
+    function handleClick(){
+        alert("Button was clicked")
+    }
+
+    const handleInput = (event) => {
+        console.clear()
+        console.log("Value :", event.target.value)
+    }
+    const name = "Aditya"
+    const name1 = "Aryan"
+    
+    const handleMouseOver = () => console.log("Mouse is Over the Text!")
+    const handleDoubleClick = () => console.log("Text Double Clicked!")
+
+    return(
+        <>
+            <h1>Hello {getName(name)}</h1>
+            <h2>Bye {getName(name1)}</h2>
+            <p onMouseOver={handleMouseOver} onDoubleClick ={handleDoubleClick}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias optio obcaecati, sequi earum quae dolor tempora fuga ullam sit, corporis vel eveniet, praesentium dignissimos quo neque mollitia! Nostrum, asperiores doloremque.</p>
+            <button onClick ={handleClick}>Click Me</button>
+            <button onClick={()=> alert("Hello from Inline Function!")}>Say Hello</button>
+            <br />
+            <input type="text" onChange = {handleInput} placeholder="Type Something" />
+        </>
+    )
 }
 
 export default hello
