524bc1a2a4465edc711be1cc9d045cd5f3654c66
Aditya-hit <adityamadissery@gmail.com>
2026-06-01 12:48:28 +0530

    Day 8

--- Express JS/First-pro/index.js
+++ Express JS/First-pro/index.js
@@ -5,9 +5,9 @@ app.listen(3000, () => {
   console.log(`succesfully server run on 3000`);
 });
 
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello Aditya</h1>");
-// });
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Aditya</h1>");
+});
 
 // app.get("/user/:userid", (req, res) => {
 //   res.send(`<h1>This is user id: ${req.params.userid}</h1>`);
